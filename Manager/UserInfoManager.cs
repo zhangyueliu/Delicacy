eb1b6c2b33fee4bdd4678ec6966c325c8a93f330
zhangyuecl <2698051674@qq.com>
2016-03-09 12:42:54 +0800

    Userinfo

--- Manager/UserInfoManager.cs
+++ Manager/UserInfoManager.cs
@@ -11,10 +11,17 @@ namespace Manager
     public class UserInfoManager
     {
         UserInfoService bll = new UserInfoService();
-        public bool Register(string loginId,string password,out string msg)
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="loginId">邮箱</param>
+        /// <param name="password">密码</param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public bool Register(string loginId, string password, out string msg)
         {
             msg = "";
-            if(string.IsNullOrEmpty(loginId)||string.IsNullOrEmpty(password))
+            if (string.IsNullOrEmpty(loginId) || string.IsNullOrEmpty(password))
             {
                 //怎么个所谓的邮箱注册
                 //if (!Regex.IsMatch(m.Username, "^[a-zA-Z0-9_]{2,20}$")) return "用户名格式不正确";
@@ -27,6 +34,31 @@ namespace Manager
             //bll.Select()
 
             return true;
+        }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="loginId">邮箱</param>
+        /// <param name="password">密码</param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public bool Login(string loginId, string password, out string msg)
+        {
+            msg = "";
+            return true;
+        }
+        /// <summary>
+        /// 修改个人信息
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
+        public bool Update(UserInfoTsfer userInfo)
+        {
+            return bll.Update(userInfo);
+        }
+        public bool Delete(UserInfoTsfer userInfo)
+        {
+            return bll.Delete(userInfo.UserId);
         }
     }
 }
