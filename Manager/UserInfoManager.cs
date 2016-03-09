5cf6b4b6a93758e602fc4b0c606d021a3c175013
zhangyuecl <2698051674@qq.com>
2016-03-09 13:24:05 +0800

    userinfo

--- Manager/UserInfoManager.cs
+++ Manager/UserInfoManager.cs
@@ -23,11 +23,6 @@ namespace Manager
             msg = "";
             if (string.IsNullOrEmpty(loginId) || string.IsNullOrEmpty(password))
             {
-                //怎么个所谓的邮箱注册
-                //if (!Regex.IsMatch(m.Username, "^[a-zA-Z0-9_]{2,20}$")) return "用户名格式不正确";
-                //if (!Regex.IsMatch(m.Password, "^[a-zA-Z0-9_]{6,20}$")) return "密码格式不正确";
-                //if (!Regex.IsMatch(m.Mobile, "^[0-9]{11}$")) return "手机格式不正确";
-                //if (!Regex.IsMatch(m.Email, @"^[0-9a-zA-Z_\-\.]+@[0-9a-zA-z_\-]+\.[0-9a-zA-Z_\-]+$")) return "邮箱格式不正确";
                 msg = "邮箱或密码不能为空";
                 return false;
             }
