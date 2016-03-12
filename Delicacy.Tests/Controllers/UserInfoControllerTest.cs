33a8b3cdb9c07b99ee5e96bb545287b767ad19d6
zhangyuecl <2698051674@qq.com>
2016-03-12 21:02:16 +0800

    完成了登录跟注册

---
+++ Delicacy.Tests/Controllers/UserInfoControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Delicacy;
+using Delicacy.Controllers;
+using System.Web.Mvc;
+using Tool;
+using Newtonsoft.Json;
+namespace Delicacy.Tests.Controllers
+{
+    [TestClass]
+    public class UserInfoControllerTest
+    {
+        UserInfoController registerController = new UserInfoController();
+        
+        
+        [TestMethod]
+        public void Register()
+        {
+            string loginId = "cl889521@163.com";
+            string pwd = "123456";
+            pwd = MD5Helper.convertToMD5(pwd);
+            
+            string contResult = registerController.Register(loginId, pwd).Content;
+          OutputModel model= JsonConvert.DeserializeObject<OutputModel>(contResult);
+          Assert.AreEqual(model.StatusCode, 1);
+          //Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        public void  Login()
+        {
+            string loginId = "cl889521@163.com";
+            string pwd = "123456";
+            pwd = MD5Helper.convertToMD5(pwd);
+            string result = registerController.Login(loginId, pwd).Content;
+            OutputModel model = JsonConvert.DeserializeObject<OutputModel>(result);
+            Assert.AreEqual(model.StatusCode, 1);
+        }
+    }
+}
---
+++ Delicacy/Controllers/UserInfoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Manager;
+using DataTransfer;
+using Tool;
+namespace Delicacy.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginId"></param>
+        /// <param name="password">md5加密后的密码</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ContentResult Register(string loginId, string password)
+        {
+            UserInfoManager userManager = new UserInfoManager();
+            OutputModel outmodel = userManager.Register(loginId, password);
+            return Content(JsonHelper.SerializeObject(outmodel));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginId"></param>
+        /// <param name="password">MD5加密后的密码</param>
+        /// <returns></returns>
+       [HttpPost]
+        public ContentResult Login(string loginId, string password)
+        {
+            UserInfoManager userManager = new UserInfoManager();
+            OutputModel outmodel = userManager.Login(loginId, password);
+            return Content(JsonHelper.SerializeObject(outmodel));
+        }
+    }
+}
