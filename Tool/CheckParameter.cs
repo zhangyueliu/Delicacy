ac4100b7c9afebefcb49baaf677471e5433ebd40
zhangyuecl <2698051674@qq.com>
2016-03-14 12:40:01 +0800

    c

--- Tool/CheckParameter.cs
+++ Tool/CheckParameter.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,26 +19,3 @@ namespace Tool
        }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tool
-{
-   public static  class CheckParameter
-    {
-       public static bool IsNullOrEmpty(params string[] param)
-       {
-           for (int i = 0; i < param.Length; i++)
-           {
-               if (string.IsNullOrEmpty(param[i]))
-                   return true;
-           }
-           return false;
-       }
-    }
-}
->>>>>>> bc713627a7c0049a3fb5b10ca0918402f23cbbd2
