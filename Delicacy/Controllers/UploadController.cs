b91ee2be74cca049496eeddb62e774f0e2ab5983
chenliang <cl889521@163.com>
2016-04-09 11:40:25 +0800

    add .

--- Delicacy/Controllers/UploadController.cs
+++ Delicacy/Controllers/UploadController.cs
@@ -26,5 +26,43 @@ namespace Delicacy.Controllers
                 return Content(OutputHelper.GetOutputResponse(ResultCode.Error));
             }
         }
+
+        public ActionResult UploadImgOne()
+        {
+            try
+            {
+                HttpFileCollectionBase files = Request.Files;
+                for (int i = 0; i < files.AllKeys.Length; i++)
+                {
+                   HttpPostedFileBase file=  files[i];
+                   string path= Server.MapPath("/Upload/Imagers/");
+                   file.SaveAs(path+"abc.jpg");
+                }
+                //var data = new { code = 200, successFileLength = 1 };
+                //JsonConvert.SerializeObject(data);
+                //return Content("<input type='hidden' value='/Upload/Imagers/abc.jpg' id='upai_url'>");//Content(UploadManager.UploadImg(files.Get("img")));
+                return View();
+            }
+            catch
+            {
+                return Content(OutputHelper.GetOutputResponse(ResultCode.Error));
+            }
+        }
+
+        public ContentResult UploadImgTwo()
+        {
+            try
+            {
+                HttpFileCollectionBase files = Request.Files;
+                var data = new { code = 200, successFileLength = 1 };
+                //JsonConvert.SerializeObject(data);
+                //return Content(JsonConvert.SerializeObject(data));//Content(UploadManager.UploadImg(files.Get("img")));
+                return Content("<input type='hidden' value='/Upload/Images/abc.jpg' id='upai_url'>");//Content(UploadManager.UploadImg(files.Get("img")));
+            }
+            catch
+            {
+                return Content(OutputHelper.GetOutputResponse(ResultCode.Error));
+            }
+        }
 	}
 }
