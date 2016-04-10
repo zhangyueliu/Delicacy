78cba1ce1c4ebc0790600089415c436a29b1f038
chenliang <cl889521@163.com>
2016-04-10 19:04:36 +0800

    添加了CookMaterialSerive.cs

---
+++ Service/CookMaterialService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF;
+using DataTransfer;
+using Tool;
+
+namespace Service
+{
+    public class CookMaterialService : BaseService<CookMaterial>
+    {
+        public void AddNoSave(List<CookMaterialTsfer> list)
+        {
+            Add(TransferObject.ConvertObjectByEntity<CookMaterialTsfer, CookMaterial>(list));
+        }
+    }
+}
