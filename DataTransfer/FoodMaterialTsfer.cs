fce08bafa2a853369d8195e37515649b4df7c20c
taohonggou <cl889521@163.com>
2016-04-20 17:39:01 +0800

    增加了食材表foodMaterial

---
+++ DataTransfer/FoodMaterialTsfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransfer
+{
+    public  class FoodMaterialTsfer
+    {
+        public int FoodMaterialId { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+    }
+}
---
+++ DataTransfer/FoodMaterial_CookBookTsfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransfer
+{
+   public  class FoodMaterial_CookBookTsfer
+    {
+        public int Id { get; set; }
+        public int FoodMaterialId { get; set; }
+        public string CookBookId { get; set; }
+    }
+}
