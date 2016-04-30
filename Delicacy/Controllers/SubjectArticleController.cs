1d20e7bdf00c2fd77ff2b6810f05bde29d8c06d1
zhangyuecl <2698051674@qq.com>
2016-04-30 14:33:38 +0800

    饮食专题页面布局

--- Delicacy/Controllers/SubjectArticleController.cs
+++ Delicacy/Controllers/SubjectArticleController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Manager;
 using Tool;
-
+using DataTransfer;
 namespace Delicacy.Controllers
 {
     public class SubjectArticleController : BaseController
@@ -23,9 +23,12 @@ namespace Delicacy.Controllers
         //}
 
 
-        public ActionResult Get(int subjectArticleId)
+        public ActionResult Get(string id)
         {
-            return Content(new SubjectArticleManager().Get(subjectArticleId));
+            int subjectArticleId;
+            int.TryParse(id, out subjectArticleId);
+            SubjectArticleTsfer s = (SubjectArticleTsfer)new SubjectArticleManager().Get(subjectArticleId).Data;
+            return View(s);
         }
 	}
 }
